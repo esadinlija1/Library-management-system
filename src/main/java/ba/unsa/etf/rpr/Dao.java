517e3054809103502a5e512e482353f23e7bac7d
esadinlija1 <esadinlija1@etf.unsa.ba>
2022-12-12 12:18:19 +0100

    Initial commit

---
+++ src/main/java/ba/unsa/etf/rpr/Dao.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr;
+
+import java.util.List;
+
+public interface Dao<T> {
+    T getById(int id);
+    T add(T item);
+    T update(T item);
+    void delete(int id);
+    List<T> getAll();
+
+}
